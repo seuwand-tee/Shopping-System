bc952774e1caf217ed9b31540cc28152ecbbb9f6
Seu Wand Tee <teewa743@student.otago.ac.nz>
2020-07-13 17:21:09 +1200

    Add Sale domain class
    References #3.

---
+++ info202_project/src/main/java/domain/Sale.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author teewa743
+ */
+public class Sale {
+	private Integer sale_id;
+	private LocalDateTime date;
+	private String status;
+
+	public Sale(Integer sale_id, LocalDateTime date, String status) {
+		this.sale_id = sale_id;
+		this.date = date;
+		this.status = status;
+	}
+	
+	public BigDecimal getTotal(){
+		BigDecimal total = new BigDecimal(0);
+		return total;
+}
+	public void addItem(SaleItem saleitem){
+		
+	}
+
+	public Integer getSale_id() {
+		return sale_id;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setSale_id(Integer sale_id) {
+		this.sale_id = sale_id;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Sale{" + "sale_id=" + sale_id + ", date=" + date + ", status=" + status + '}';
+	}
+	
+}
