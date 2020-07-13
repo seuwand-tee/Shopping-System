8353331e0c6a37b2cd7060d40b0c09828be54ca9
Seu Wand Tee <teewa743@student.otago.ac.nz>
2020-07-13 17:21:41 +1200

    Add SaleItem domain class
    References #4.

---
+++ info202_project/src/main/java/domain/SaleItem.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author teewa743
+ */
+public class SaleItem {
+	private BigDecimal quantity_purchased;
+	private BigDecimal sale_price;
+
+	public SaleItem(BigDecimal quantity_purchased, BigDecimal sale_price) {
+		this.quantity_purchased = quantity_purchased;
+		this.sale_price = sale_price;
+	}
+	
+		public BigDecimal getItemTotal(){
+			
+		return sale_price.multiply(quantity_purchased);
+		}
+
+	public BigDecimal getQuantity_purchased() {
+		return quantity_purchased;
+	}
+
+	public BigDecimal getSale_price() {
+		return sale_price;
+	}
+
+	public void setQuantity_purchased(BigDecimal quantity_purchased) {
+		this.quantity_purchased = quantity_purchased;
+	}
+
+	public void setSale_price(BigDecimal sale_price) {
+		this.sale_price = sale_price;
+	}
+
+	@Override
+	public String toString() {
+		return "SaleItem{" + "quantity_purchased=" + quantity_purchased + ", sale_price=" + sale_price + '}';
+	}
+	
+		
+}
