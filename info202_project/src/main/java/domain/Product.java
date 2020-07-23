dd07cb92627cd91a7a710a342877565dfd1440ea
Seu Wand Tee <teewa743@student.otago.ac.nz>
2020-07-24 10:47:25 +1200

    Changed the toString() of Product domain References #1

--- info202_project/src/main/java/domain/Product.java
+++ info202_project/src/main/java/domain/Product.java
@@ -83,7 +83,7 @@ public class Product {
 
 	@Override
 	public String toString() {
-		return "Product{" + "product_id=" + product_id + ", name=" + name + ", description=" + description + ", category=" + category + ", list_price=" + list_price + ", quantity_in_stock=" + quantity_in_stock + '}';
+		return "Product ID= " + product_id + ", name= " + name;
 	}
 
 	
