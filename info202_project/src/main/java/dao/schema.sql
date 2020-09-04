f5b5cd226671e8f77672c9ec695b0e4d8df4b8ac
Seu Wand Tee <teewa743@student.otago.ac.nz>
2020-09-04 15:59:52 +1200

    Customer sql created References #28

--- info202_project/src/main/java/dao/schema.sql
+++ info202_project/src/main/java/dao/schema.sql
@@ -9,13 +9,13 @@ quantityInStock decimal(6,2) not null,
 constraint Product_PK primary key (productID)
 );
 create table Customer(
-customer_id int(10) not null,
-username varchar(50) not null,
+customer_id int(10) auto_increment(1000),
+username varchar(50) not null unique,
 firstname varchar(50) not null,
 surname varchar(50) not null,
-password varchar(50) not null,
+password varchar(100) not null,
 email_address varchar(100) not null,
-shipping_adddress varchar(100) not null,
+shipping_adddress varchar(500) not null,
 constraint Customer_PK primary key (customer_id)
 );
 
