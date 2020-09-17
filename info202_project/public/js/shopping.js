9eb6fd01b6eee9c35036a60bffcbc015981c593d
User <User@LAPTOP-B7R0KD45.fritz.box>
2020-09-17 23:54:44 +1200

    AngularJS done on products and create account html #30

--- info202_project/public/js/shopping.js
+++ info202_project/public/js/shopping.js
@@ -3,9 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-/* global $window, signInAPI, $sessionStorage */
+/* global $window, signInAPI, $sessionStorage, sessionData */
 
 "use strict";
+class SaleItem {
+
+    constructor(product, quantity) {
+        // only set the fields if we have a valid product
+        if (product) {
+            this.product = product;
+            this.quantity_purchased = quantity;
+            this.sale_price = product.list_price;
+        }
+    }
+
+    getItemTotal() {
+        return this.sale_price * this.quantity_purchased;
+    }
+
+}
+class ShoppingCart {
+
+    constructor() {
+        this.items = new Array();
+    }
+
+    reconstruct(sessionData) {
+        for (let item of sessionData.items) {
+            this.addItem(Object.assign(new SaleItem(), item));
+        }
+    }
+
+    getItems() {
+        return this.items;
+    }
+
+    addItem(item) {
+        this.items.push(item);
+    }
+
+    setCustomer(customer) {
+        this.customer = customer;
+    }
+
+    getTotal() {
+        let total = 0;
+        for (let item of this.items) {
+            total += item.getItemTotal();
+        }
+        return total;
+    }
+
+}
 // create a new module, and load the other pluggable modules
 var module = angular.module('ShoppingApp', ['ngResource', 'ngStorage']);
 module.factory('productAPI', function ($resource) {
@@ -64,4 +113,25 @@ this.signIn = function (username, password) {
 });
 module.factory('signInAPI', function ($resource) {
    return $resource('/api/customers/:username');
+});
+
+module.factory('cart', function ($sessionStorage) {
+    let cart = new ShoppingCart();
+
+    // is the cart in the session storage?
+    if ($sessionStorage.cart) {
+
+        // reconstruct the cart from the session data
+        cart.reconstruct($sessionStorage.cart);
+    }
+
+    return cart;
+});
+module.controller('ShoppingCartController', function (cart) {
+this.items = cart.getItems();
+this.total = cart.getTotal();
+function(product){
+    $sessionStorage.cart = product;
+    this.selectedProduct = $sessionStorage.selectedProduct;
+}
 });
